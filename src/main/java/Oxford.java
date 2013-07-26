5bd460f8d10de3930433eee174537bfdbfa03c94
MapR Technologies <mapr@mapr-desktop.(none)>
2013-07-26 16:44:13 -0700

    Agregando oxford

--- src/main/java/Oxford.java
+++ src/main/java/Oxford.java
@@ -21,28 +21,22 @@ public class Oxford{
         public void map(LongWritable key, Text value,
         OutputCollector<Text, Text> output, Reporter reporter)
         throws IOException {
-            try{
-                String line = value.toString();
-                String year = line.substring(15, 19) ;
-                String month =  line.substring(19, 21) ;
-				String day =  line.substring(21, 23) ;
-                int airTemperature;
-                if (line.charAt(87) == '+') { // parseInt doesn't like leading plus signs
-                  airTemperature = Integer.parseInt(line.substring(88, 92));
-                } else {
-                  airTemperature = Integer.parseInt(line.substring(87, 92));
-                }
+			String line = value.toString();
+			try{
+				String year = line.substring(3, 7);
+				StringTokenizer tokens = new StringTokenizer( line );
 				
-				String quality = line.substring(92, 93);
-				if (airTemperature != MISSING && quality.matches("[01459]")) {
-					output.collect( new Text( ""+year+"-"+month ), 
-						new Text( day + "/" + month + "/" + year + "@" + airTemperature ) );
+				int int_year = Integer.parseInt( tokens.nextToken() );
+				String month = String.format( "%02d", Integer.parseInt( tokens.nextToken() ) );
+				String temp_max = tokens.nextToken();
+				
+				if( int_year >= 2008 && int_year <= 2012 ){
+					output.collect( new Text( year + "-" + month ), 
+						new Text( "dd/" + month + "/" + year + "@" + temp_max ) ); 
 				}
-
-                
-            }catch( Exception e){
-                
-            }
+				
+			}catch( Exception e){
+			}
         }
     }
   
@@ -53,24 +47,10 @@ public class Oxford{
         public void reduce(Text key, Iterator<Text> values,
         OutputCollector<Text, Text> output, Reporter reporter)
         throws IOException {
-            
-			String date_max = "";
-			int max_temp = Integer.MIN_VALUE;
+            String value = values.next().toString();
+			value = value.replace("@", "," );
 			
-            while( values.hasNext() ){
-				String[] date_temp = values.next().toString().split("@");
-				String date = date_temp[0];
-				int temp = Integer.parseInt( date_temp[1] );
-				if( temp > max_temp){
-					max_temp = temp;
-					date_max = date;
-				}
-            }
-			
-			float air_temp = ( (float) max_temp ) / 10;
-			String air_temp_str = String.format("%2.02f", air_temp);
-            
-            output.collect( new Text( ciudad + "-" + key),  new Text( date_max + "," + air_temp_str ));
+			output.collect( new Text( ciudad + "-" + key),  new Text( value ) );
             //output.collect( new Text( ciudad + "-" + key), new Text( max_month + "," + max_tmp ) );
         }
     }
